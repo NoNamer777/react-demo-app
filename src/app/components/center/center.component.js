8317bddb496799f3dbc7765bb0e08f82613ab384
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-27 17:03:03 +0100

    Contain the center content

--- src/app/components/center/center.component.js
+++ src/app/components/center/center.component.js
@@ -2,7 +2,7 @@ import './center.component.scss';
 
 const CenterComponent = () => {
     return (
-        <main className="flex-grow-1 flex-shrink-1">
+        <main className="flex-grow-1 flex-shrink-1 container py-2">
             <p>center-component works!</p>
         </main>
     );
