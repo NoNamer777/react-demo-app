fcd78e5dedad5d24f9a292072eafc6dc2d2d7f21
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-27 20:43:05 +0100

    Remove unused bootstrap variable

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import AppComponent from './app/app.component';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
-const bootstrap = require('bootstrap');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
