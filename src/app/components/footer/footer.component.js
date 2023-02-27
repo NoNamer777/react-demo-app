f66e34ea95f1edfb9792ed18a3a940200ba5bf4e
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-27 17:02:54 +0100

    Add basic layout for the footer

--- src/app/components/footer/footer.component.js
+++ src/app/components/footer/footer.component.js
@@ -2,8 +2,13 @@ import './footer.component.scss';
 
 const FooterComponent = () => {
     return (
-        <footer className="flex-grow-0 flex-shrink-0">
-            <p>footer-component works!</p>
+        <footer className="flex-grow-0 flex-shrink-0 d-flex flex-wrap justify-content-between align-items-center py-3 border-top bg-body-tertiary">
+            <div className="col-md-4 d-flex align-items-center">
+                <a className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                    <img src="assets/images/logo.png" alt="logo" />
+                </a>
+                <span className="mb-3 mb-md-0 text-muted">© 2023 DnD Mapp</span>
+            </div>
         </footer>
     );
 };
