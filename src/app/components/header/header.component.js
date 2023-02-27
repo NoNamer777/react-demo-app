fb98e6ccae36b6c97fcc8695b7dacbdb9d4e8295
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-27 16:52:40 +0100

    Add basic layout for the header

--- src/app/components/header/header.component.js
+++ src/app/components/header/header.component.js
@@ -3,7 +3,29 @@ import './header.component.scss';
 const HeaderComponent = () => {
     return (
         <header className="flex-grow-0 flex-shrink-0">
-            <p>header-component works!</p>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand">
+                        <img src="assets/images/logo.png" alt="logo" />
+                        DnD Mapp - Races
+                    </a>
+                    <button
+                        type="button"
+                        className="navbar-toggler"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbar-supported-content"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbar-supported-content">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link">Overview</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
     );
 };
