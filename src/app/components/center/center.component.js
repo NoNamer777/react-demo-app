7b65ceb33d79d3ee48f131ea85998dc8b71a6cb6
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-01 12:32:45 +0100

    Show pagination in sticky footer
    Only on medium devices or smaller

--- src/app/components/center/center.component.js
+++ src/app/components/center/center.component.js
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import PaginationComponent from '../header/pagination/pagination.component';
 
 const CenterComponent = () => {
     return (
@@ -6,6 +7,9 @@ const CenterComponent = () => {
             <article className="container py-2 h-100">
                 <Outlet />
             </article>
+            <div className="d-flex justify-content-center d-lg-none sticky-bottom py-2 bg-body-tertiary border-top">
+                <PaginationComponent />
+            </div>
         </main>
     );
 };
