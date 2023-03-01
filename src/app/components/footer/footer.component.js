2b9a33c674c0bf157358657a4d39a6bd84ad1036
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-01 10:57:13 +0100

    Fix footer increasing in height
    When the window got smaller

--- src/app/components/footer/footer.component.js
+++ src/app/components/footer/footer.component.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 const FooterComponent = () => {
     return (
         <footer className="flex-grow-0 flex-shrink-0 d-flex flex-wrap justify-content-between align-items-center py-3 border-top bg-body-tertiary">
-            <div className="col-md-4 d-flex align-items-center">
-                <Link to="/" className="mb-3 mx-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <div className="d-flex align-items-center">
+                <Link to="/" className="mx-2 text-muted text-decoration-none lh-1">
                     <img src="assets/images/logo.png" alt="logo" className="logo" />
                 </Link>
-                <span className="mb-3 mb-md-0 text-muted">© 2023 DnD Mapp</span>
+                <span className="text-muted">© 2023 DnD Mapp</span>
             </div>
         </footer>
     );
