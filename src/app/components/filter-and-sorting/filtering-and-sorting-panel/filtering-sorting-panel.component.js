b1cf95bcfb4839d17fc7a962e5f349f4be6006a5
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-02 15:49:37 +0100

    Add basic filtering and sorting panel

---
+++ src/app/components/filter-and-sorting/filtering-and-sorting-panel/filtering-sorting-panel.component.js
@@ -0,0 +1,36 @@
+const FilteringSortingPanelComponent = () => {
+    return (
+        <div className="offcanvas offcanvas-end" tabIndex="-1" id="filtering-sorting-panel">
+            <div className="offcanvas-header border-bottom">
+                <h5 className="offcanvas-title">Filtering and Sorting</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <form className="offcanvas-body d-flex flex-column gap-3">
+                <div className="mb-3">
+                    <label className="form-label" htmlFor="attribute-sorting-select">
+                        Sort by attribute
+                    </label>
+                    <select className="form-select" id="attribute-sorting-select">
+                        <option value=""></option>
+                    </select>
+                </div>
+                <div className="mb-auto">
+                    <label htmlFor="trait-filter-select" className="form-label">
+                        Filter by Trait:
+                    </label>
+                    <select className="form-select" id="trait-filter-select">
+                        <option value=""></option>
+                    </select>
+                </div>
+                <button type="reset" className="btn btn-danger">
+                    Reset
+                </button>
+                <button type="submit" className="btn btn-success">
+                    Apply
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default FilteringSortingPanelComponent;
