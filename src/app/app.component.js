e155118fbec79511a40e6ac150c118f780e29476
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-03 11:06:33 +0100

    Update the page title based on the active route

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import CenterComponent from './components/center/center.component';
 import FooterComponent from './components/footer/footer.component';
 import HeaderComponent from './components/header/header.component';
 
 const AppComponent = () => {
+    const location = useLocation();
+
+    const routeTitleMap = {
+        '/': '',
+        '/overview': ': Overview',
+    };
+    const titlePrefix = 'DnD Mapp - Races';
+
+    useEffect(() => {
+        document.title = titlePrefix + routeTitleMap[location.pathname];
+    }, [location.pathname]);
+
     return (
         <>
             <HeaderComponent />
