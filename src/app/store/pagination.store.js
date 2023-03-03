089bc89775455922b469941893d287bd7c56fd52
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-03 11:46:25 +0100

    Remove unused pagination actions

--- src/app/store/pagination.store.js
+++ src/app/store/pagination.store.js
@@ -22,20 +22,11 @@ export const paginationSlice = createSlice({
         setTotalPages: (state, action) => {
             state.totalPages = Math.ceil(action.payload / 5);
         },
-        gotToPreviousPage: (state) => {
-            if (state.page === 1) return;
-            state.page--;
-        },
-        goToNextPage: (state) => {
-            if (state.page === state.totalPages) return;
-            state.page++;
-        },
         goToPage: (state, action) => {
             state.page = action.payload;
         },
     },
 });
 
-export const { setTotalPages, setSorting, gotToPreviousPage, goToNextPage, goToPage } = paginationSlice.actions;
 
 export default paginationSlice.reducer;
